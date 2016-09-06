6c9cc9fb7496e015ef4dcb8ba1c26797b51d0046
ragnyll <justanotherjakeyg@gmail.com>
2016-09-06 01:33:17 -0500

    next node is being added to stack, but it is not in the correct string format to parse right

--- AI/AStarSearch/PacmanDFS.py
+++ AI/AStarSearch/PacmanDFS.py
@@ -11,21 +11,21 @@ stack = []
 
 # push the starting node
 stack.append(pacman_position)
-# print(graph[int(current_node[0])][int(current_node[1])]) Verifies pacman starting position
 
 # push the visitable neigbors
 while len(stack) > 0:
+    print(stack)
     current_node = stack.pop(0)
     print(current_node)
     # look up
     if graph[int(current_node[0]) + 1][int(current_node[1])] == '-':
-        stack.append(graph[int(current_node[0]) + 1][int(current_node[1])])
+        stack.append([str(int(current_node[0]) + 1) + ', ' + str(current_node[1])])
     #look left
     if graph[int(current_node[0])][int(current_node[1]) - 1] == '-':
-        stack.append(graph[int(current_node[0])][int(current_node[1]) - 1])
-    #look right FIX THIS FIRST
+        stack.append([str(current_node[0]) + ', ' + str(int(current_node[1]) - 1)])
+    #look right
     if graph[int(current_node[0])][int(current_node[1]) + 1] == '-':
-        stack.append()
+        stack.append([str(current_node[0]) + ', ' + str(int(current_node[1]) + 1)])
     #look down
     if graph[int(current_node[0]) - 1][int(current_node[1])] == '-':
-        stack.append(graph[int(current_node[0]) - 1][int(current_node[1])])
+        stack.append([str(int(current_node[0]) -1 ) + ', ' + str(current_node[1])])
