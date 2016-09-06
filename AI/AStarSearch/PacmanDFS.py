649df865a0f8958c04624da5304b1f52f749f279
ragnyll <justanotherjakeyg@gmail.com>
2016-09-06 00:54:44 -0500

    looks correctly on first iteration. needs to get ordered pair of next place to look. fix right first

--- AI/AStarSearch/PacmanDFS.py
+++ AI/AStarSearch/PacmanDFS.py
@@ -1,11 +1,31 @@
 pacman_position = input().split()
 food_position = input().split()
-# graph dimensions = [rows, column]
 graph_dimensions = input().split()
 
 graph = []
 for rows in range(0, int(graph_dimensions[0])):
     graph.append(list(input()))
 
-for row in graph:
-    print(row)
+nodes_explored = 0
+stack = []
+
+# push the starting node
+stack.append(pacman_position)
+# print(graph[int(current_node[0])][int(current_node[1])]) Verifies pacman starting position
+
+# push the visitable neigbors
+while len(stack) > 0:
+    current_node = stack.pop(0)
+    print(current_node)
+    # look up
+    if graph[int(current_node[0]) + 1][int(current_node[1])] == '-':
+        stack.append(graph[int(current_node[0]) + 1][int(current_node[1])])
+    #look left
+    if graph[int(current_node[0])][int(current_node[1]) - 1] == '-':
+        stack.append(graph[int(current_node[0])][int(current_node[1]) - 1])
+    #look right FIX THIS FIRST
+    if graph[int(current_node[0])][int(current_node[1]) + 1] == '-':
+        stack.append()
+    #look down
+    if graph[int(current_node[0]) - 1][int(current_node[1])] == '-':
+        stack.append(graph[int(current_node[0]) - 1][int(current_node[1])])
